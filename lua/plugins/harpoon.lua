12cee13914499adf53ab6e6f302541efca48f869
AlexNg <taboez4life@gmail.com>
2024-02-23 21:58:31 +0800

    feat: Migration to harpoon2 w/ telescope and better keymapping

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,19 +2,117 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
     },
-    config = true,
-    keys = {
-      { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-      { "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+    config = function()
+      local harpoon = require("harpoon")
 
-      { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon buffer 1" },
-      { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon buffer 2" },
-      { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon buffer 3" },
-      { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Harpoon buffer 4" },
+      ---@diagnostic disable-next-line: missing-parameter, missing-fields
+      harpoon.setup({})
+
+      -- Telescope Config
+      local config = require("telescope.config").values
+      local function toggle_telescope(files)
+        local paths = {}
+        for _, i in ipairs(files.items) do
+          table.insert(paths, i.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({ results = paths }),
+            previewer = config.file_previewer({}),
+            sorter = config.generic_sorter({}),
+          })
+          :find()
+      end
+
+      -- Quick Access Keybinds
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():append()
+      end, { desc = "Add file to harpoon" })
+
+      vim.keymap.set("n", "<leader>1", function()
+        harpoon:list():select(1)
+      end, { desc = "Harpoon buffer 1" })
+
+      vim.keymap.set("n", "<leader>2", function()
+        harpoon:list():select(2)
+      end, { desc = "Harpoon buffer 2" })
+
+      vim.keymap.set("n", "<leader>3", function()
+        harpoon:list():select(3)
+      end, { desc = "Harpoon buffer 3" })
+
+      vim.keymap.set("n", "<leader>4", function()
+        harpoon:list():select(4)
+      end, { desc = "Harpoon buffer 4" })
+
+      -- Harpoon Keybinds
+      vim.keymap.set("n", "<leader>hh", function()
+        toggle_telescope(harpoon:list())
+      end, { desc = "Show harpoon marks" })
+
+      vim.keymap.set("n", "<leader>ha", function()
+        harpoon:list():append()
+      end, { desc = "Add file to harpoon" })
+
+      vim.keymap.set("n", "<leader>h1", function()
+        harpoon:list():select(1)
+      end, { desc = "Harpoon buffer 1" })
+
+      vim.keymap.set("n", "<leader>h2", function()
+        harpoon:list():select(2)
+      end, { desc = "Harpoon buffer 2" })
+
+      vim.keymap.set("n", "<leader>h3", function()
+        harpoon:list():select(3)
+      end, { desc = "Harpoon buffer 3" })
+
+      vim.keymap.set("n", "<leader>h4", function()
+        harpoon:list():select(4)
+      end, { desc = "Harpoon buffer 4" })
+
+      vim.keymap.set("n", "<leader>hd1", function()
+        harpoon:list():remove(1)
+      end, { desc = "Delete harpoon buffer 1" })
+
+      vim.keymap.set("n", "<leader>hd2", function()
+        harpoon:list():remove(2)
+      end, { desc = "Delete harpoon buffer 2" })
+
+      vim.keymap.set("n", "<leader>hd3", function()
+        harpoon:list():remove(3)
+      end, { desc = "Delete harpoon buffer 3" })
+
+      vim.keymap.set("n", "<leader>hd4>", function()
+        harpoon:list():remove(4)
+      end, { desc = "Delete harpoon buffer 4" })
+
+      vim.keymap.set("n", "<leader>hdd", function()
+        harpoon:list():clear()
+      end, { desc = "Delete all harpoon buffers" })
+    end,
+  },
+
+  -- Add <leader>h descriptions
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>h"] = { name = "+harpoon" },
+        ["<leader>hd"] = { name = "+delete" },
+      },
     },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
   },
 }

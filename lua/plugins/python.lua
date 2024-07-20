144ecd0451e6f531f7a27384fd774341142f1950
AlexNg <contact@ngjx.org>
2024-07-20 18:17:08 +0800

    fix: Python venv config

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -20,17 +20,19 @@ return {
   -- Venv selector
   {
     "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    config = function() end,
-    setup = function() end,
-    opts = {
-      name = "venv",
-      auto_refresh = true,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    event = "VeryLazy",
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
+    end,
     keys = {
-      { "<leader>vs", "<cmd>VenvSelect<cr>" },
-      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+      { ",v", "<cmd>VenvSelect<cr>" },
     },
   },
 }

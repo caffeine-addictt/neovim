dea7ee403ea3c5836862822014ea3b6813ad00fd
AlexNg <contact@ngjx.org>
2024-07-20 18:14:59 +0800

    feat: Treat .ejs as html

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "*.ejs",
+  callback = function()
+    vim.cmd([[set filetype=html]])
+  end,
+})

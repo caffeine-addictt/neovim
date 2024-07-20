c752a21af81ee5c3c8889e7a3faf5f63c15c60bf
AlexNg <contact@ngjx.org>
2024-07-20 18:16:20 +0800

    chore: Remove commented

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,16 +1,3 @@
--- Auto Enable venv selector
-vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
-  end,
-  once = true,
-})
-
 return {
   -- LSP Configuration
   {

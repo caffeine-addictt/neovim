58c8c975903dd7fe735e42d4b9ca68eb49c942ba
AlexNg <contact@ngjx.org>
2024-07-22 18:23:01 +0800

    fix: Patch edge case where venv does not auto activate

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector.cached_venv").retrieve()
+    end
+  end,
+  once = true,
+})
+
 return {
   -- LSP Configuration
   {
